c406967e6a64e84bd5523d91dd98bda177a8c233
Zachary Kline <thefuzzyfish2@gmail.com>
2020-02-21 10:39:37 -0600

    Finalized database initialization

--- src/database.java
+++ src/database.java
@@ -1,15 +1,16 @@
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.File;
+import java.sql.*;
 
 /**
  * Represents a database instance. This is the man in between everything else and the backend database, implementing in JDBC.
  * @author Zachary Kline <www.ZachKline.us>
  */
 public class database {
-    private String dbname;
-    private String dbpath;
+    private String dbName;
+    private String dbPath;
+    private String dirPath;
+    private PreparedStatement statement;
+    private Connection dbcon;
     static final String USER = "moneybuddy";
     static final String PASS = "moneybuddy";
 
@@ -19,35 +20,73 @@ public class database {
      */
     public database(String name) {
         /* -- Sets up path variables -- */
-        dbname = name;
-        dbpath = System.getProperty("user.home");
+        dbName = name;
+        dbPath = System.getProperty("user.home");
+        dirPath = System.getProperty("user.home");
+        File f; // Used to create a new folder if necessary
 
         if (System.getProperty("os.name").equals("Windows")) {
-            dbpath = dbpath.concat("\\.MoneyBuddy\\" + name + ".db"); //Sets the path to use forward slashes for Windows file paths
-            dbpath = "jdbc:sqlite:" + dbpath;
+            dbPath = dbPath.concat("\\.MoneyBuddy\\" + name + ".db"); //Sets the path to use forward slashes for Windows file paths
+            dirPath = dirPath.concat("\\.MoneyBuddy\\");
+            dbPath = "jdbc:sqlite:" + dbPath;
         } else if (System.getProperty("os.name").equals("Linux")) {
-            dbpath = dbpath.concat("/.MoneyBuddy/" + name + ".db"); //Sets the path to use backslashes for Linux file paths (the better way)
-            dbpath = "jdbc:sqlite:" + dbpath;
+            dbPath = dbPath.concat("/.MoneyBuddy/" + name + ".db"); //Sets the path to use backslashes for Linux file paths (the better way)
+            dirPath = dirPath.concat("/.MoneyBuddy/");
+            dbPath = "jdbc:sqlite:" + dbPath;
         }
-        /* -- End setting up path variable -- */
 
-        //FIXME - Make sure you create ~/.MoneyBuddy/ with mkdir() if it doesn't exist
+        /* -- Create directory if it doesn't already exist -- */
+        f = new File(dirPath);
+        f.mkdir();
+        /* -- End creating directory -- */
 
         /* -- Sets up database connection -- */
-        try (Connection con = DriverManager.getConnection(dbpath)) { //Tries to establish a connection to the database
-            System.out.println("The driver name is " + con.getMetaData().getDriverName());
-            System.out.println("A new database has been created.");
-        } catch (SQLException e) { //If connection to database is unsuccessful, print error
-            System.out.println(e.getMessage());
+        dbcon = null;
+        try {
+            dbcon = DriverManager.getConnection(dbPath);
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
         /* -- End setting up database connection -- */
+
+        /* -- If this is a new database, initializes the database structure. Else, does nothing -- */
+        try {
+            statement = dbcon.prepareStatement(
+                    "CREATE TABLE IF NOT EXISTS transactions(id INT AUTO_INCREMENT PRIMARY_KEY, date DATE NOT NULL, amount FLOAT NOT NULL, memo VARCHAR(255), need TINYINT NOT NULL)"
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            statement.execute(); //Actually runs the prepared statement
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        /* -- End setting up database structure -- */
     }
 
     /**
      * Accessor; Returns the SQL database path;
-     * @return folder path of the SQL directory
+     * @return path of the SQLite DB
      */
-    public String getPath() {
-        return dbpath;
+    public String getDbPath() {
+        return dbPath;
+    }
+
+    /**
+     * Accessor; Returns the program data path
+     * @return folder path of the program data; where everything persistent is stored
+     */
+    public String getDirPath() {
+        return dirPath;
+    }
+
+    /**
+     * Accessor; Returns database name
+     * @return name of the active database
+     */
+    public String getDbName() {
+        return dbName;
     }
 }
