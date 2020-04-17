916b0115c344aa8c6028add891ada72c8f47c47e
Zachary Kline <thefuzzyfish2@gmail.com>
2020-04-17 10:58:02 -0500

    Fixed an unnecessary NullPointerException

--- src/helpers.java
+++ src/helpers.java
@@ -1,19 +1,24 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 
-
+/**
+ *  Feel free to throw some bloat-code in here if you want to clean up your functions
+ */
 public class helpers {
     /**
-     * List the databases already associated with the active user
+     * List the databases already associated with the active user. Be sure that the output array is not empty before you start parsing the array.
      * @return the databases already associated with the active user
      */
     public static String[] dbList() {
         File folder = new File(System.getProperty("user.home") + "/.MoneyBuddy/");
         String[] filenames = folder.list();
 
-        /* Skim the file extensions off the filenames */
-        for (int i = 0; i < filenames.length; i++) {
-            filenames[i] = filenames[i].substring(0, filenames[i].lastIndexOf('.'));
-        }
+        if (filenames != null) { // This would sometimes produce a NullPointerException otherwise
+            /* Skim the file extensions off the filenames */
+            for (int i = 0; i < filenames.length; i++) {
+                filenames[i] = filenames[i].substring(0, filenames[i].lastIndexOf('.'));
+            }
+       }
 
         return filenames;
     }
